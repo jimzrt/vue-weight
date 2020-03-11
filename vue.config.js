d62f68ecd0ee40420e30fd46f0402853f4d90e3b
jimzrt <james.tophoven@gmail.com>
2020-03-11 02:04:52 +0100

    remove webpack analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,4 @@
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-  .BundleAnalyzerPlugin;
 
 module.exports = {
   transpileDependencies: ["vue-echarts", "resize-detector"],
-  configureWebpack: {
-    plugins: [new BundleAnalyzerPlugin()]
-  }
 };
